810962f8b7f0f9a376ff9d79c9e57e33fbb4ec4f
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2019-03-22 20:06:08 -0700

    Edit README to check off MVP items; organize dbHelpers code

--- data/dbHelpers.js
+++ data/dbHelpers.js
@@ -12,6 +12,8 @@ module.exports = {
   addAction
 }
 
+
+// Projects helper functions ------------------------
 // retrieve project by id number
 function getProjectById(id){
   return db('projects')
@@ -24,18 +26,23 @@ function getProjects(){
   return db('projects')
 }
 
-// retrieve actions for given project id
-function getActionsByProjectId(id){
-  return db('actions')
-    .where({project_id: Number(id)});
-}
-
 // add new project
 function addProject(project){
   return db('projects')
     .insert(project)
     .then(ids => ({id: ids[0]}));
 }
+
+
+
+// Actions helper functions ------------------------
+
+// retrieve actions for given project id
+function getActionsByProjectId(id){
+  return db('actions')
+    .where({project_id: Number(id)});
+}
+
 
 // add new action
 function addAction(action){
